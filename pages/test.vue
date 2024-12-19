4c6d4202571c93da86fad5c5b34a6cc3b3d299fe
k-karen <k-karen@users.noreply.github.com>
2024-12-19 17:05:34 +0900

    x

--- pages/test.vue
+++ pages/test.vue
@@ -4,19 +4,12 @@
       <h1 class="text-3xl font-bold text-center">Test Page</h1>
       <p class="text-center">This is a test page {{ random ?? '' }}</p>
     </div>
-    <Suspense>
-      <template #default>
-        <AsyncEx />
-      </template>
-    </Suspense>
+    
+    <ex />
   </div>
 </template>
 
 <script setup>
-import { defineAsyncComponent } from 'vue'
-
-const AsyncEx = defineAsyncComponent(() => import('./components/ex.vue'))
-
 const random = ref(null)
 const fetchRandomV2 = async () => {
   const { data } = await useFetch('http://localhost:4567/random_v2?test=2')
